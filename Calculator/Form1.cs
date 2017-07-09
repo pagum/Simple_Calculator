ee993740f870ff1e799e9243e4ede5e283d78a4e
Paulina Gumienna <paugumienna@gmail.com>
2017-07-09 20:35:51 +0200

    first commit

---
+++ Calculator/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        double value = 0;
+        string operation = "";
+        bool operation_pressed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if ((result.Text == "0") || (operation_pressed))
+            {
+                result.Clear();
+            }
+            operation_pressed = false;
+            Button b = (Button)sender;
+            result.Text = result.Text + b.Text;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+        }
+
+        private void operator_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            operation = b.Text;
+            value = double.Parse(result.Text);
+            operation_pressed = true;
+            equation.Text = value + "" + operation;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            operation_pressed = false;
+            equation.Text = "";
+            switch (operation)
+            {
+                case "+":
+                    result.Text=(value + double.Parse(result.Text)).ToString();
+                    break;
+                case "-":
+                    result.Text = (value - double.Parse(result.Text)).ToString();
+                    break;
+                case "*":
+                    result.Text = (value * double.Parse(result.Text)).ToString();
+                    break;
+                case "/":
+                    result.Text = (value / double.Parse(result.Text)).ToString();
+                    break;
+                default:
+                    break; 
+            }
+            
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            result.Text="0";
+            value = 0;
+        }      
+    }
+}
